faa6fbec2fba4261fc2b06ba1038cfcba8ef0c13
Moulitharan M <moulitharan4080@gmail.com>
2025-11-16 20:32:41 +0530

    Add files via upload

---
+++ calculator app.py
@@ -0,0 +1,51 @@
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    if b==0:
+        return "Error:Division by zero is not allowed!"
+    return a/b
+def modulus(a,b):
+    return a%b
+def power(a,b):
+    return a**b
+
+def calculator():
+    
+    print("Read README for better understanding")
+
+
+while True:
+    print("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Power\n7.Exit")
+
+    choice=input("Select operation(1/2/3/4/5/6/7):")
+    if choice=="7":
+        print("Exiting")
+        break
+    if choice not in ('1','2','3','4','5','6'):
+        print("Invalid option! Please enter a valid option")
+        continue
+    try:
+        num1=float(input("Enter first number:"))
+        num2=float(input("Enter second number:"))
+    except ValueError:
+        print("Invalid input! Please enter numeric values")
+        continue
+    if choice=="1":
+        print("Result",add(num1,num2))
+    elif choice=="2":
+        print("Result",subtract(num1,num2))
+    elif choice=="3":
+        print("Result",multiply(num1,num2))
+    elif choice=="4":
+        print("Result",divide(num1,num2))
+    elif choice=='5':
+        print("Result",modulus(num1,num2))
+    elif choice=='6':
+        print("Result",power(num1,num2))
+    print("+ + + + + + + + + + + +")
+    
+calculator()
